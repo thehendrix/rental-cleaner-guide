87783d4fedfbe452714ce2628fe66ed4cf0a8c8a
Marc Hendrikse <Marc.Hendrikse@virginorbit.com>
2024-09-15 17:40:19 -0700

    feat: add project baseline

---
+++ app/spaces/[spaceId]/page.tsx
@@ -0,0 +1,5 @@
+export default function SpacePage({ params }) {
+  return (
+    <h1>Space { params.spaceId }</h1>
+  );
+}
---
+++ app/spaces/page.tsx
@@ -0,0 +1,10 @@
+export default function SpacesPage() {
+  return (
+    <>
+      <h1>Spaces List</h1>
+      <h2>Space 1</h2>
+      <h2>Space 2</h2>
+      <h2>Space 3</h2>
+    </>
+  );
+}
