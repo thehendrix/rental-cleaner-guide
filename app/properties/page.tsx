cf1e9a2dad6527ae3e4b2156fe808883f0d58603
Marc Hendrikse <Marc.Hendrikse@virginorbit.com>
2024-09-26 12:26:53 -0700

    fix: Handling Missing Properties

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -4,14 +4,19 @@ import properties from '../../properties.json'
 export default function PropertiesPage() {
   return (
     <>
-      <h1>Properties List</h1>
-      <ul>
-        {properties.map((property) => (
-          <li key={property.id}>
-            <Link href={`/properties/${property.id}`}>{property.name} in {property.location.city}, {property.location.state}</Link>
-          </li>
-        ))}
-      </ul>
+      {properties.length ?
+      <>
+        <h1>Properties List</h1>
+        <ul>
+          {properties.map((property) => (
+            <li key={property.id}>
+              <Link href={`/properties/${property.id}`}>{property.name} in {property.location.city}, {property.location.state}</Link>
+            </li>
+          ))}
+        </ul>
+      </>
+      : <h1>No Properties Exist</h1>
+      }
     </>
   );
 }
